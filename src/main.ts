61cb48a9a568401b9c72153a8e1d6da4b733ff49
Matheus Comparotto <matheusdanielgomess@gmail.com>
2022-04-25 13:50:56 -0400

    Corrigida <input>

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { hideContainer, load, showCoracaoPartido } from './components/loaderComp
 import { showVideos } from './components/videoComponent';
 import api from './services/api';
 
-const app = <HTMLDivElement>document.getElementById('app');
+const $ = document.querySelector.bind(document);
+
+const app = <HTMLDivElement>$('#app');
+const input = <HTMLInputElement>$(`input[name='search']`);
 
 const searchVideos = async (search: string) => {
   load();
@@ -32,4 +35,5 @@ const search = url.searchParams.get('search');
 
 if (search) {
   searchVideos(search);
+  input.value = search;
 }
